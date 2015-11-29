9fb8e9839e5f07c17b33c43e489c14b28deac515
Nicolas Broeking <nbroeking@me.com>
2015-11-29 15:29:53 -0700

    Started working on getting started

--- src/FoundationsApp/app/src/main/java/main/foundationsapp/OurApplication.java
+++ src/FoundationsApp/app/src/main/java/main/foundationsapp/OurApplication.java
@@ -1,8 +1,69 @@
 package main.foundationsapp;
 
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+
 /**
  * Created by nbroeking on 11/29/15.
  */
 public class OurApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    public OurApplication() {
+        super();
+    }
+
+    @Override
+    public void registerComponentCallbacks(ComponentCallbacks callback) {
+        super.registerComponentCallbacks(callback);
+    }
+
+    @Override
+    public void unregisterComponentCallbacks(ComponentCallbacks callback) {
+        super.unregisterComponentCallbacks(callback);
+    }
+
+    @Override
+    public void registerActivityLifecycleCallbacks(ActivityLifecycleCallbacks callback) {
+        super.registerActivityLifecycleCallbacks(callback);
+    }
+
+    @Override
+    public void registerOnProvideAssistDataListener(OnProvideAssistDataListener callback) {
+        super.registerOnProvideAssistDataListener(callback);
+    }
+
+    @Override
+    public void unregisterOnProvideAssistDataListener(OnProvideAssistDataListener callback) {
+        super.unregisterOnProvideAssistDataListener(callback);
+    }
+
+    @Override
+    public void unregisterActivityLifecycleCallbacks(ActivityLifecycleCallbacks callback) {
+        super.unregisterActivityLifecycleCallbacks(callback);
+    }
 }
