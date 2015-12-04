1a0b236518c7d5665b889db971bd5896399e8d9f
Nicolas Broeking <nbroeking@me.com>
2015-12-03 18:55:09 -0700

    Added Async Task

--- src/FoundationsApp/app/src/main/java/main/foundationsapp/MainActivity.java
+++ src/FoundationsApp/app/src/main/java/main/foundationsapp/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends Activity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
     }
 
 
--- src/FoundationsApp/app/src/main/java/main/foundationsapp/MyService.java
+++ src/FoundationsApp/app/src/main/java/main/foundationsapp/MyService.java
@@ -2,11 +2,46 @@ package main.foundationsapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.IBinder;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class MyService extends Service {
+
+    private Executor executor = Executors.newSingleThreadExecutor();
+
+
     public MyService() {
 
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                //DO whatever the intent said to do!!!!!!!
+            }
+        });
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
